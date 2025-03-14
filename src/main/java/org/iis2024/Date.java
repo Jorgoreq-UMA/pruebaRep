d7a835d3a3ab8a1f0484cb9f1bc3e1cfeabf5073
Jorgoreq-UMA <jorgoreq0112@gmail.com>
2025-03-14 14:08:06 +0100

    Added a new method to Date.java

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,6 +20,10 @@ public class Date {
   private final int month;
   private final int year;
 
+// Owner is Jorge Gómez Requerey
+  public void methodAddedByOwner(){
+    System.out.println("Method addded by the owner");
+  }
   /**
    * Retrieves the day component of this date.
    *
